d8f0b5aefc1fd9077a751d71750901ea9cf550ae
RobsonSena <robsonsena@gmail.com>
2019-09-15 00:22:05 -0300

    commit inicial java script avançado

---
+++ client/js/index.js
@@ -0,0 +1,37 @@
+
+
+Date.prototype.toDateInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+});
+document.getElementById('data').value = new Date().toDateInputValue();
+
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#quantidade'),
+    document.querySelector('#valor') 
+];
+
+var tbody = document.querySelector('table tbody');
+
+document.querySelector('.form').addEventListener('submit', function(event) {
+
+    event.preventDefault();
+
+    var tr = document.createElement('tr');
+
+    campos.forEach(function(campo) {
+        var td = document.createElement('td');
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+
+    var tdVolume = document.createElement('td');
+    tdVolume.textContent = campos[1].value * campos[2].value;
+
+    tr.appendChild(tdVolume);
+
+    tbody.appendChild(tr);
+
+});
